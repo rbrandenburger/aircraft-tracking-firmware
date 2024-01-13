63d9779cebabb8176c119c552bb9b208c3264838
Remington Brandenburger <rbrandenburger@huskers.unl.edu>
2024-01-13 17:51:08 -0600

    Catch and Log Exceptions in Processor

--- processor.py
+++ processor.py
@@ -1,10 +1,10 @@
+import logger
+import re
 from utilites.general import table_loader
 from utilites import (
     hex_broadcast_decoder,
     coordinate_decoder,
 )
-
-import re
 
 
 class Processor:
@@ -14,26 +14,35 @@ class Processor:
         aircraftLookupTable = table_loader.get_table("registeredAircraftTable.csv")
 
         while True:
-            hexBroadcast, timestamp = hexBroadcastQueue.get()
-            hexBroadcast = re.sub(r'[^0-9a-fA-F]', '', hexBroadcast.upper())
-            broadcast = hex_broadcast_decoder.generate_broadcast_from_hex(hexBroadcast, aircraftLookupTable)
+            try:
+                hexBroadcast, timestamp = hexBroadcastQueue.get()
+                hexBroadcast = re.sub(r'[^0-9a-fA-F]', '', hexBroadcast.upper())
+                broadcast = hex_broadcast_decoder.generate_broadcast_from_hex(hexBroadcast, aircraftLookupTable)
 
-            # When the captured radio broadcast is not an ADSB packet
-            if (broadcast is None):
-                continue
-
-            broadcast.timestamp = timestamp
-
-            # If it was a positional packet, add to positional array and run positional decoder
-            if broadcast.payload['typeCode'] in coordinate_decoder.POSITIONAL_TYPECODES:
-                incompletePositionalBroadcasts.append(broadcast)
-                broadcasts, incompletePositionalBroadcasts = coordinate_decoder.decode_positions(incompletePositionalBroadcasts)
-
-                if (len(broadcasts) == 0):
+                # When the captured radio broadcast is not an ADSB packet
+                if (broadcast is None):
                     continue
 
-                for broadcast in broadcasts:
+                broadcast.timestamp = timestamp
+
+                # If it was a positional packet, add to positional array and run positional decoder
+                if broadcast.payload['typeCode'] in coordinate_decoder.POSITIONAL_TYPECODES:
+                    incompletePositionalBroadcasts.append(broadcast)
+                    broadcasts, incompletePositionalBroadcasts = coordinate_decoder.decode_positions(incompletePositionalBroadcasts)
+
+                    if (len(broadcasts) == 0):
+                        continue
+
+                    for broadcast in broadcasts:
+                        processedBroadcastQueue.put(broadcast)
+
+                else:
                     processedBroadcastQueue.put(broadcast)
 
-            else:
-                processedBroadcastQueue.put(broadcast)
+            except Exception as e:
+                # Append broadcast data to error message
+                msg = "\nBroadcast: {}".format(hexBroadcast)
+                e.args = (e.args[0] + msg,) + e.args[1:]
+
+                logger.log_error(e)
+                continue
